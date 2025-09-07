d4e391947cbc21521b2931d92d073fbd1dedf8ce
Michael Solis <michael.solis.alarcon@gmail.com>
2025-09-07 18:37:13 -0300

    cambios en index (titulo)

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-
 let amigos = [];
 
 // Utilizar const cuando la variable no cambia ej; nombres
